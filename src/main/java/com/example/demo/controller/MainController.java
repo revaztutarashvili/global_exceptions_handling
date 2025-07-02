a45b79fee1900aeb742c30bc4466dd4ac32fed5b
revaztutarashvili <generationofjeans@gmail.com>
2025-07-03 03:13:25 +0400

    check requests

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.PersonRequest;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,5 +16,20 @@ public class MainController {
     @PostMapping("/save")
     public void savePerson(){
         throw new IllegalArgumentException("illegal argument provided");
+    }
+    @PostMapping("/createUser")
+    public void createPerson(@Valid @RequestBody PersonRequest personRequest){
+        System.out.println("New person received and validated. Username: " + personRequest.getUserName());
+        System.out.println( "Use created successfully:"+
+                personRequest.getUserName() +
+                personRequest.getPersonalNumber() +
+                personRequest.getPassword() +
+                personRequest.getName()+
+                personRequest.getMail()) ;
+
+
+
+
+
     }
 }
--- src/main/java/com/example/demo/model/PersonRequest.java
+++ src/main/java/com/example/demo/model/PersonRequest.java
@@ -1,27 +1,63 @@
-//package com.example.demo.model;
-//
-//import jakarta.validation.constraints.*;
-//
-//public class PersonRequest {
-//    @NotNull
-//    @NotBlank(message = "name field required")
-//    private String name;
-//
-//    @NotBlank(message = "PN should not be blank or less than 11 digit")
-//    @Size(min = 11, max = 11, message = "pn should contain 11 digits")
-//    private int personalNumber;
-//
-//    @Min(value = 6, message = "password must be contain 6 or more character")
-//    private String password;
-//
-//    @NotBlank(message = "username field required")
-//    @Max(value = 10, message = "username max letter is 10")
-//    private String userName;
-//
-//    @Email
-//    @NotBlank(message = "field required to be filled up")
-//    private String mail;
-//
-//
-//
-//}
+package com.example.demo.model;
+
+import jakarta.validation.constraints.*;
+
+public class PersonRequest {
+    @NotNull (message = "name should not be null")
+    private String name;
+
+    @NotBlank(message = "Required")
+    @Min(value = 11, message = "pn should contain 11 digits")
+    @Max(value = 11, message = "pn should contain 11 digits")
+    private int personalNumber;
+
+    @Size(min = 6, max = 12, message = "password must contain 6-12 character")
+    private String password;
+
+    @NotBlank(message = "required")
+    @Size(min = 6, max = 12, message = "username should 4-12 letter")
+    private String userName;
+
+    @Email(message = "field required to be filled up")
+    private String mail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public void setPersonalNumber(int personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
